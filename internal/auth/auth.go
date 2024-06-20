7c710f5b063d53de39f75031d7ab28f291360e1b
pmanrique001 <pmanrique001@gmail.com>
2024-06-20 16:18:09 -0400

    Initial project structure.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,4 +1,4 @@
-package main
+package auth
 
 import (
 	"fmt"
@@ -6,27 +6,16 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
-func loadEnv() {
-	// Load environment variables from .env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-}
-
-func getAccount() {
-	// Make a GET request to the Alpaca API to verify connection
+// GetAccount makes a GET request to the Alpaca API to verify connection.
+func GetAccount() {
 	url := "https://" + os.Getenv("APISERVER_DOMAIN") + "/v2/account"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatalf("Error creating request: %v", err)
 	}
 
-	// Set headers with API keys
 	req.Header.Set("APCA-API-KEY-ID", os.Getenv("API_KEY"))
 	req.Header.Set("APCA-API-SECRET-KEY", os.Getenv("API_SECRET"))
 
@@ -35,20 +24,17 @@ func getAccount() {
 	if err != nil {
 		log.Fatalf("Error making request: %v", err)
 	}
-	defer resp.Body.Close()
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Fatalf("Error closing response body: %v", err)
+		}
+	}()
 
-	// Read response
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalf("Error reading response: %v", err)
 	}
 
-	// Print response
 	fmt.Println("Response status:", resp.Status)
 	fmt.Println("Response body:", string(body))
-}
-
-func main() {
-	loadEnv()    // Load environment variables
-	getAccount() // Make a GET request to verify connection
 }
